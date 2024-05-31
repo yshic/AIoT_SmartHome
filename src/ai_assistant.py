286ca705d79cb885fa4f99df87e0abddf475d29d
yshic <tuanl799@gmail.com>
2024-05-31 13:46:11 +0700

    change: system instruction

--- src/ai_assistant.py
+++ src/ai_assistant.py
@@ -46,12 +46,18 @@ class AIAssistant:
             safety_settings=safety_settings,
             generation_config=generation_config,
             system_instruction=(
-                "You are an AI assistant for a smart home. You can control the light (there's only 1 light for now), "
-                "the fan, the door. You can also give the sensor (temperature, humidity and light) data. "
-                "You can converse like a normal assistant but keep it short and sweet, if a command is given, only check if it belongs "
-                "to the above tasks. When being asked to turn on the fan, please ask for the fan speed first, "
-                "after that, the user will usually response with a percentage value. Then set the fan to this "
-                " value. When being asked for update on the status of the house, ALWAYS response in the format of: STATUS: ..."
+                "You are an helpful AI assistant for a smart home. You can control the light (there's only 1 light for now), the fan and the door. "
+                "You can also give the sensor data (temperature, humidity and light). "
+                "You can converse like a normal assistant but if a command is given, only check if it belongs "
+                "to the above tasks. When being asked to turn on the fan (in normal working mode), please ask for the fan speed first, "
+                "after that, the user will response with a percentage value. Then set the fan to this value. When being asked for "
+                "update on the status of the house, ALWAYS response in the format of: STATUS: ..."
+                "The system will enter a special mode when the gesture control system is on, the user can ask you to this mode on or off. "
+                "Remember that if the gesture control system is on, the system will now works like a state machine, if the light is "
+                "currently on and the command related to the light is given, the light will be turn off and vice versa BY YOU without the need " 
+                "for the user to confirm whether to turn it on or off, you need to announce this out loud for the user, this is unrelated "
+                "to the act of updating the status of the house. The same applies to the door, HOWEVER, the FAN don't need to be alternate "
+                "between on and off as there are 5 gesture related to 5 level of speed for the fan so you just need to set the fan to that level."
             ),
         )
         return model
